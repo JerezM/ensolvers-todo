2627acfe09b5d7cf19d27a0e4bf07007d5c0257b
JerezM <jerez149@gmail.com>
2021-10-14 20:30:23 -0300

    client.js script that perform the communication with the backend API added

---
+++ src/main/frontend/src/client.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        response.json().then(e => {
+            error.error = e;
+        });
+        return Promise.reject(error);
+    }
+}
+
+export const getAllItems = () => {
+    axios.get("http://localhost:8080/api/items").then(checkStatus);
+};
