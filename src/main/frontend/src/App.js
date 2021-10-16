de27008e7de3d6c447e3d112cf602e391e0d9d3a
JerezM <jerez149@gmail.com>
2021-10-16 17:56:39 -0300

    App.js basic structure added

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import TodoList from './components/todo-list-component';
+
+function App() {
+    return(
+        <div className="app">
+            <TodoList />
+        </div>
+    );    
+}
+
+export default App;
