432cf11dc6c50a65fa6875381aa8da76030d9067
JerezM <jerez149@gmail.com>
2021-10-16 21:01:45 -0300

    compose added to store.js

--- src/main/frontend/src/store.js
+++ src/main/frontend/src/store.js
@@ -1,16 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 
-const initialState = {};
+const initialState = {
+  items: []
+};
 
 const middleware = [thunk];
 
 const store = createStore(
   reducers,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  compose(applyMiddleware(thunk),
+    typeof window === 'object' &&
+        typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
+          window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )
+  /*initialState,
+  composeWithDevTools(applyMiddleware(...middleware))*/
 );
 
 export default store;
