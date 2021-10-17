75d2e16d381f9f4114ffbfe1d1076a73ce78750f
JerezM <jerez149@gmail.com>
2021-10-16 22:31:48 -0300

    Router added to Item.js

--- src/main/frontend/src/Item.js
+++ src/main/frontend/src/Item.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { markItem, deleteItem } from "./client"
-import { Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import { UpdateItemForm } from './forms/UpdateItemForm'
+import { Todo } from './Todo'
 
 class Item extends React.Component {
 
@@ -30,16 +38,33 @@ class Item extends React.Component {
     }
 
     render() {
+        const { id, content } = this.state;
+        
         return (
-            
-            <div className="todo-item">
-                <input type="checkbox" checked={this.state.isMarked} onChange={this.updateIsMarked}/>{" "}
-                {this.state.content !== undefined ? 
-                    <p>{this.state.content}</p> : null
-                }
-                <Link to="/"><button className="btn-edit">Edit</button>{" "}</Link>
-                <button className="btn-remove" onClick={this.handleClickDeleteItem}>Remove</button>
-            </div>
+            <Router>
+                <div className="todo-item">
+                    <input type="checkbox" checked={this.state.isMarked} onChange={this.updateIsMarked}/>{" "}
+                    {this.state.content !== undefined ? 
+                        <p>{this.state.content}</p> : null
+                    }
+                    <Link to="/items/edit"><button className="btn-edit">Edit</button>{" "}</Link>
+                    <button className="btn-remove" onClick={this.handleClickDeleteItem}>Remove</button>
+                </div>
+
+                <Switch>
+                    <Route path={["/", "/items"]}>
+                        <Todo/>
+                    </Route>
+                    <Route path="/items/edit">
+                        <UpdateItemForm 
+                            key = {id}
+                            id = {id}
+                            itemContent = {content}
+                            onSuccess = {this.props.onSuccess}
+                        />
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
 }
